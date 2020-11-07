6251ae9b3affb726d2120eca82060628f66c3c02
Chinh Nguyen <chinhht09@gmail.com>
2020-11-07 20:17:22 +0700

    Complete Browse part 2

--- screens/Browse.js
+++ screens/Browse.js
@@ -1,7 +1,15 @@
 import React from "react";
-import { Image, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+  Dimensions,
+} from "react-native";
 import { theme, mocks } from "../constants";
-import { Button, Block, Text } from "../components";
+import { Button, Block, Text, Card, Badge } from "../components";
+
+const { width } = Dimensions.get("window");
 export default class Browse extends React.Component {
   state = {
     active: "Products",
@@ -23,7 +31,7 @@ export default class Browse extends React.Component {
     );
   }
   render() {
-    const { profile } = this.props;
+    const { profile, navigation, categories } = this.props;
     const tabs = ["Products", "Inspirations", "Shop"];
     return (
       <Block>
@@ -38,6 +46,36 @@ export default class Browse extends React.Component {
         <Block flex={false} row style={styles.tabs}>
           {tabs.map((tab) => this.renderTab(tab))}
         </Block>
+
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          style={{ paddingVertial: theme.sizes.base * 2 }}
+        >
+          <Block flex={false} row space="between" style={styles.categories}>
+            {categories.map((category) => (
+              <TouchableOpacity
+                key={category.name}
+                onPress={() => navigation.navigate("Explore", { category })}
+              >
+                <Card center middle shadow style={styles.category}>
+                  <Badge
+                    margin={[0, 0, 15]}
+                    size={50}
+                    color="rgba(41,216,143,0.20)"
+                  >
+                    <Image source={category.image} />
+                  </Badge>
+                  <Text medium height={20}>
+                    {category.name}
+                  </Text>
+                  <Text gray caption>
+                    {category.count} products
+                  </Text>
+                </Card>
+              </TouchableOpacity>
+            ))}
+          </Block>
+        </ScrollView>
       </Block>
     );
   }
@@ -45,6 +83,7 @@ export default class Browse extends React.Component {
 
 Browse.defaultProps = {
   profile: mocks.profile,
+  categories: mocks.categories,
 };
 
 const styles = StyleSheet.create({
@@ -68,5 +107,16 @@ const styles = StyleSheet.create({
   active: {
     borderBottomColor: theme.colors.secondary,
     borderBottomWidth: 3,
+  },
+  category: {
+    // this should be dynamic based on screen width
+    minWidth: (width - theme.sizes.padding * 2.4 - theme.sizes.base) / 2,
+    maxWidth: (width - theme.sizes.padding * 2.4 - theme.sizes.base) / 2,
+    maxHeight: (width - theme.sizes.padding * 2.4 - theme.sizes.base) / 2,
+  },
+  categories: {
+    flexWrap: "wrap",
+    paddingHorizontal: theme.sizes.base * 2,
+    marginBottom: theme.sizes.base * 3.5,
   },
 });
